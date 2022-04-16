d31f527363b56a67b14764156b81055faf6f4df3
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-15 19:39:27 -0600

    Final Commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,9 +2,6 @@ class Author < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :birth_year
 
-  # has_many :invoice_items
-  # has_many :items, through: :invoice_items
-
   has_many :book_authors
   has_many :books, through: :book_authors
 
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe Author, type: :model do
     it { should have_many :book_authors }
     it { should have_many :books }
   end
+
+  describe 'methods' do
+    before do
+      @author_1 = Author.create!(
+        name: "Smart Happy Guy",
+        birth_year: DateTime.new(1987)
+      )
+      @author_2 = Author.create!(
+        name: "Great Silly Guy",
+        birth_year: DateTime.new(2002)
+      )
+      @author_3 = Author.create!(
+        name: "Not A Good Guy",
+        birth_year: DateTime.new(1887)
+      )
+    end
+
+    describe 'instance methods' do
+      it '#age calculates age based on birth year' do
+        expect(@author_1.age).to eq(35)
+        expect(@author_2.age).to eq(20)
+        expect(@author_3.age).to eq(135)
+      end
+    end
+  end
 end
