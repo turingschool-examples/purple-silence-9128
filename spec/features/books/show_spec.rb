f580442cec3f08342bcd48de3324459f41cfe9a2
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-17 14:54:05 -0600

    Test: add feature test for user story 2, book show page having the authors average age

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -15,15 +15,27 @@ RSpec.describe 'Book Show Page' do
             book_author2 = BookAuthor.create!(book: dark, author: bach)
             
             visit "/books/#{dark.id}"
-            save_and_open_page
+            # save_and_open_page
             expect(page).to have_content("Dark Tower")
             expect(page).to have_content("fantasy")
             expect(page).to have_content("Author Name: stephen king")
             expect(page).to have_content("Author Name: richard bachman")
             expect(page).to have_content("At Library: The Library")
+            expect(page).to_not have_content("Other Library")
 
+        end
 
-
+        it 'Has the average age of the authors' do
+                       lib = Library.create!(name: 'The Library')
+            lib2 = Library.create!(name: 'Other Library')
+            king = Author.create!(name: 'stephen king', age: 77)
+            bach = Author.create!(name: 'richard bachman', age: 55)
+            dark = Book.create!(title: 'Dark Tower', genre: 'fantasy', library_id: lib.id)
+            book_author1 = BookAuthor.create!(book: dark, author: king)
+            book_author2 = BookAuthor.create!(book: dark, author: bach)
+            
+            visit "/books/#{dark.id}"
+            expect(page).to have_content("Authors Average Age: 66")
         end
 
 
