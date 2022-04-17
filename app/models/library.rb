5cdc9b9f04863fb20011a25b650c39f33e513c62
James Harkins <jh.harkins@protonmail.com>
2022-04-17 16:38:54 -0600

    Feat: pass model tests for Libraries model .most_popular_authors instance method

--- app/models/library.rb
+++ app/models/library.rb
@@ -2,4 +2,12 @@ class Library < ApplicationRecord
   validates_presence_of :name
   has_many :books
   has_many :authors, through: :books
+
+  def most_popular_authors
+    authors.joins(:book_authors)
+           .select('authors.*')
+           .group('authors.id')
+           .order('COUNT(book_authors.author_id = authors.id) DESC')
+           .limit(3)
+  end
 end
