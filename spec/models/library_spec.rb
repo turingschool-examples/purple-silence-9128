812899721d78d1a39e132717d55c5aec95b466f4
Alex Psitos <apsitos08@gmail.com>
2022-04-17 19:53:57 -0400

    Test: libary relationships tested and passed

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe Library, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
-  
+
   describe 'relationships' do
     it { should have_many :books }
+    it { should have_many :author_books }
+    it { should have_many :authors }
   end
 end 
