4e0b905d406ebff08e2c4189e50d24d7b6ca3be7
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-17 16:01:07 -0600

    Refactor: add more robust testing to book show_spec.rb

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -14,14 +14,29 @@ RSpec.describe 'Book Show Page' do
             book_author1 = BookAuthor.create!(book: dark, author: king)
             book_author2 = BookAuthor.create!(book: dark, author: bach)
             
+            
+            fake = Author.create!(name: 'fake mcfake', age: 7)
+            the_stand = Book.create!(title: 'The Stand', genre: 'horror', library_id: lib.id)
+            book_author3 = BookAuthor.create!(book: the_stand, author: king)
+            book_author4 = BookAuthor.create!(book: the_stand, author: fake)
+          
+           
             visit "/books/#{dark.id}"
             # save_and_open_page
             expect(page).to have_content("Dark Tower")
             expect(page).to have_content("fantasy")
-            expect(page).to have_content("Author Name: stephen king")
-            expect(page).to have_content("Author Name: richard bachman")
             expect(page).to have_content("At Library: The Library")
+
             expect(page).to_not have_content("Other Library")
+            expect(page).to_not have_content("The Stand")
+            expect(page).to_not have_content("fake mcfake")
+
+            within "#author-#{king.id}" do
+                expect(page).to have_content("Author Name: stephen king")
+            end
+            within "#author-#{bach.id}" do
+                expect(page).to have_content("Author Name: richard bachman")
+            end
 
         end
 
@@ -37,9 +52,6 @@ RSpec.describe 'Book Show Page' do
             # save_and_open_page
             expect(page).to have_content("Authors Average Age: 66")
         end
-
-
-
 
     end 
 end
