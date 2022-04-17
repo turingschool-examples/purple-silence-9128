296ea072729544e4ff93d82498fa1342a85505d7
Andrew Speth <speth.andrew@yahoo.com>
2022-04-17 09:32:36 -0600

    relationship tests set up

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Book, type: :model do
   
   describe 'relationships' do
     it { should belong_to :library }
+    it { should have_many :author_books }
+    it { should have_many(:authors).through(:author_books) }
   end 
 end
