05b11d2b6670ee30afa2c4f0ab9f18d0a66d4ed8
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-18 16:42:05 -0600

    Story 1 complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
   belongs_to :library
   has_many :author_books
   has_many :authors, through: :author_books
+
+  # def author_names
+  #   authors = Book.joins(:author_books, :authors).where(id: @book.id).first.authors
+  #   authors.map { |author| author.name }
+  # end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,8 +7,19 @@ RSpec.describe Book, type: :model do
   end
 
   describe "relationships" do
-    it { should belong_to :library }
-    it { should have_many :author_books }
+    it { should belong_to(:library) }
+    it { should have_many(:author_books) }
     it { should have_many(:authors).through(:author_books) }
   end
+
+  # let!(:dpl) { Library.create!(name: "Denver Public Library") }
+  # let!(:herbology) { Book.create!(title: "A Guide to Herbology", genre: "Nonfiction", library_id: dpl.id) }
+  # let!(:henderson) { Author.create!(name: "Dee Henderson", birth_year: 1959) }
+  # let!(:goshawk) { Author.create!(name: "Gerald Goshawk", birth_year: 1948) }
+  # let!(:author_book_herbology) { AuthorBook.create!(book_id: herbology.id, author_id: henderson.id) }
+  # let!(:author_book_herbology) { AuthorBook.create!(book_id: herbology.id, author_id: goshawk.id) }
+  #
+  # it "returns all authors names for a book" do
+  #   expect(herbology.author_names).to eq(["Dee Henderson", "Gerald Goshawk"])
+  # end
 end
