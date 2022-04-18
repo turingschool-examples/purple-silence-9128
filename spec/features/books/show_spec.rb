ea9c1caddc9382ae4dbb62526586554af6d7ef72
Will Medders <wmedders@gmail.com>
2022-04-18 14:31:11 -0400

    Test: books show displays title, genre, authors names, and library name

---
+++ spec/features/books/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Books Show' do
+  it 'displays the books name, genre, authors names, and library name' do
+    lib_1 = Library.create!(name: 'Library of Book Learnin')
+    lib_2 = Library.create!(name: 'The Little Library')
+    author_1 = Author.create!(name: 'Bob Simmons', birth_year: 1934)
+    author_2 = Author.create!(name: 'Jennifer Marley', birth_year: 1961)
+    author_3 = Author.create!(name: 'Jason Scripp', birth_year: 1990)
+    book_1 = lib_1.books.create!(title: "How to Get Rich Quick", genre: "Self Help")
+
+    visit "books/#{book_1.id}"
+
+    expect(page).to have_content("How to Get Rich Quick")
+    expect(page).to have_content("Bob Simmons")
+    expect(page).to have_content("Jennifer Marley")
+    expect(page).to have_no_content("Jason Scripp")
+    expect(page).to have_content("Library of Book Learnin")
+    expect(page).to have_no_content("The Little Library")
+  end
+end
