56aa5412c0ec68314877840d6f767c9fc8ace543
Will Medders <wmedders@gmail.com>
2022-04-18 15:14:56 -0400

    Feat: libraries show displays the libraries name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:show]
+
+  resources :libraries, only: [:show]
 end
