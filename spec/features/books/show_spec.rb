73b6ddaa9d3f9c5190d9973e188aba376985b1ab
Alex Psitos <apsitos08@gmail.com>
2022-04-17 20:24:12 -0400

    Test: add multiple authors to one book and test for their presence on the show page

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe 'the book show page' do
     @book_2 = @library_1.books.create!(title: "Book Two", genre: "Horror")
     @book_3 = @library_2.books.create!(title: "No Title", genre: "No Genre")
     @author_1 = Author.create!(name: "Amanda Hugankiss", birth_year: DateTime.new(1984))
-    @author_2 = Author.create!(name: "Mr. Good", birth_year: DateTime.new(1950))
+    @author_2 = Author.create!(name: "Fanny McPatdown", birth_year: DateTime.new(1950))
     @author_3 = Author.create!(name: "Mr. Evil", birth_year: DateTime.new(1969))
     @author_book_1 = AuthorBook.create!(author_id: @author_1.id, book_id: @book_1.id)
-    @author_book_2 = AuthorBook.create!(author_id: @author_2.id, book_id: @book_2.id)
+    @author_book_2 = AuthorBook.create!(author_id: @author_2.id, book_id: @book_1.id)
     @author_book_3 = AuthorBook.create!(author_id: @author_3.id, book_id: @book_3.id)
     visit "/books/#{@book_1.id}"
   end
@@ -19,5 +19,14 @@ RSpec.describe 'the book show page' do
   it 'shows the books name and genre' do
     expect(page).to have_content("Book One")
     expect(page).to have_content("Finance")
+    expect(page).to_not have_content("Book Two")
+    expect(page).to_not have_content("Horror")
+  end
+
+  it 'shows the books authors' do
+    within('#authors') do
+      expect(page).to have_content("Amanda Hugankiss")
+      expect(page).to have_content("Fanny McPatdown")
+    end
   end
 end
