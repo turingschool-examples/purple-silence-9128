9157de58b6f59b78c63f56c75646bb31f4bb1d88
Will Medders <wmedders@gmail.com>
2022-04-18 14:01:41 -0400

    Create tables Authors and BookAuthors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_161250) do
+ActiveRecord::Schema.define(version: 2022_04_18_180013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.integer "birth_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.bigint "books_id"
+    t.bigint "authors_id"
+    t.index ["authors_id"], name: "index_book_authors_on_authors_id"
+    t.index ["books_id"], name: "index_book_authors_on_books_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,5 +44,7 @@ ActiveRecord::Schema.define(version: 2022_04_11_161250) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "book_authors", "authors", column: "authors_id"
+  add_foreign_key "book_authors", "books", column: "books_id"
   add_foreign_key "books", "libraries"
 end
