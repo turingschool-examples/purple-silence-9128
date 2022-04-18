fa4f7087d92f09f12e09a2133ab3c7a8a4a0d96e
Will Medders <wmedders@gmail.com>
2022-04-18 14:49:37 -0400

    Test: book show displays the average age of the authors

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Books Show' do
     BookAuthor.create!(book_id: "#{book_1.id}", author_id: "#{author_2.id}")
 
     visit "books/#{book_1.id}"
-    
+
     expect(page).to have_content("How to Get Rich Quick")
     expect(page).to have_content("Bob Simmons")
     expect(page).to have_content("Jennifer Marley")
@@ -20,4 +20,19 @@ RSpec.describe 'Books Show' do
     expect(page).to have_content("Library of Book Learnin")
     expect(page).to have_no_content("The Little Library")
   end
+
+  it 'displays the average age of the authors' do
+    lib_1 = Library.create!(name: 'Library of Book Learnin')
+    lib_2 = Library.create!(name: 'The Little Library')
+    author_1 = Author.create!(name: 'Bob Simmons', birth_year: 1934)
+    author_2 = Author.create!(name: 'Jennifer Marley', birth_year: 1961)
+    author_3 = Author.create!(name: 'Jason Scripp', birth_year: 1990)
+    book_1 = lib_1.books.create!(title: "How to Get Rich Quick", genre: "Self Help")
+    BookAuthor.create!(book_id: "#{book_1.id}", author_id: "#{author_1.id}")
+    BookAuthor.create!(book_id: "#{book_1.id}", author_id: "#{author_2.id}")
+
+    visit "books/#{book_1.id}"
+
+    expect(page).to have_content("74.5")
+  end    
 end
