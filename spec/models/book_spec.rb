05cc663927198a1b14b2f24c525dc32dfcdc6fe0
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-17 21:45:43 -0600

    Test: Book model finds average age of authors

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  before :all do
+    @library = Library.create!(name: "Big Ass Library")
+
+    @book_1 = @library.books.create!(title: "Put This Book Down and Do Your IC", genre: "Nonfiction")
+    @book_2 = @library.books.create!(title: "For God's Sake Don't Read This", genre: "Fiction")
+
+    @thiccums = Author.create!(name: "H.T. Thiccums", birth_year: 1904)
+    @hiccups = Author.create!(name: "J.R. Hiccups", birth_year: 2017)
+    @bad_author = Author.create!(name: "I.Am. Terrible", birth_year: 1847)
+    @strange_author = Author.create!(name: "Wiggles McDoofus", birth_year: 1975)
+
+    @ab_1 = AuthorBook.create!(author_id: @thiccums.id, book_id: @book_1.id)
+    @ab_2 = AuthorBook.create!(author_id: @hiccups.id, book_id: @book_1.id)
+    @ab_3 = AuthorBook.create!(author_id: @bad_author.id, book_id: @book_2.id)
+    @ab_4 = AuthorBook.create!(author_id: @strange_author.id, book_id: @book_2.id)
+  end
+
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :genre }
@@ -13,8 +30,8 @@ RSpec.describe Book, type: :model do
   end
 
   describe "instance methods" do
-    it "text" do
-
+    it "finds average age of authors" do
+      expect(@book_1.avg_authors_age).to eq(146.5)
     end
   end
 end
