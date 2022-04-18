cdd442c0b6e6170266522d9751ae5bfe88d02931
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-17 22:55:49 -0600

    Test: Library show page lists 3 most popular authors

--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -30,11 +30,26 @@ RSpec.describe "Library Show Page" do
 
   it "has link to view all authors that have books in the library" do
     click_link "Authors At This Library"
-    
+
     expect(current_path).to eq("/libraries/#{@library.id}/authors")
     expect(page).to have_content("H.T. Thiccums")
     expect(page).to have_content("J.R. Hiccups")
     expect(page).to have_content("I.Am. Terrible")
     expect(page).to have_content("Wiggles McDoofus")
+  end
+
+  it "lists three most popular authors" do
+    book_3 = @library.books.create!(title: "This Book is Sooo Good", genre: "Nonfiction")
+    book_4 = @library.books.create!(title: "Hello your FUTURE'S ON THE LINE", genre: "Fiction")
+
+    ab_5 = AuthorBook.create!(author_id: @thiccums.id, book_id: book_3.id)
+    ab_6 = AuthorBook.create!(author_id: @hiccups.id, book_id: book_4.id)
+    ab_7 = AuthorBook.create!(author_id: @bad_author.id, book_id: book_4.id)
+
+    within("#popular_authors") do
+      expect(page).to have_content(@thiccums.name)
+      expect(page).to have_content(@hiccups.name)
+      expect(page).to have_content(@bad_author.name)
+    end
   end
 end
