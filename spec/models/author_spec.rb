f33026bdb730b28e8625a584be20c2e097ad382c
Kim G <xiaole.guo@yahoo.com>
2022-04-18 11:46:32 -0600

    average age shown as integer

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Author, type: :model do
 
   describe 'class methods' do 
     describe '#average_author_age' do 
-      it 'calculates the average author age to 1/10 decimal' do 
+      it 'calculates the average author age' do 
         author1 = Author.create!(name: 'Stephen King', birth_year: 1947)
         author2 = Author.create!(name: 'Peter Straub', birth_year: 1943)
         author3 = Author.create!(name: 'Cixin Liu', birth_year: 1963)
