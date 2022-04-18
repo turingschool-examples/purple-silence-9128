cb12a3f0ee75631ad91ea51e2f7785118e9a287a
Will Medders <wmedders@gmail.com>
2022-04-18 16:09:26 -0400

    FEAT: model Author #three_most_popular

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,4 +5,8 @@ class Author < ApplicationRecord
   def self.avg_age
     average('2022 - birth_year')
   end
+
+  def self.three_most_popular
+    joins(:books).select('authors.*, sum(books.id) as num').group('authors.id').order('num desc').limit(3)
+  end
 end
