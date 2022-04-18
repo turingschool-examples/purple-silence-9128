470f4f342ce598486af01540acae937b46966a31
Alex Psitos <apsitos08@gmail.com>
2022-04-17 22:15:08 -0400

    Final commit

--- app/models/library.rb
+++ app/models/library.rb
@@ -3,4 +3,13 @@ class Library < ApplicationRecord
   has_many :books
   has_many :author_books, through: :books
   has_many :authors, through: :author_books
+
+
+  def three_most_popular_authors
+    authors.joins(:books)
+           .select('authors.*, count(books) as all_books')
+           .group('authors.id')
+           .order(all_books: :desc)
+           .limit(3)
+  end
 end
--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -4,16 +4,32 @@ RSpec.describe 'the library show page' do
   before do
     @library_1 = Library.create!(name: "Richmond Public Library")
     @library_2 = Library.create!(name: "Henrico Public Library")
+
     @book_1 = @library_1.books.create!(title: "Book One", genre: "Finance")
     @book_2 = @library_1.books.create!(title: "Book Two", genre: "Horror")
     @book_3 = @library_2.books.create!(title: "No Title", genre: "No Genre")
+    ### Extension books
+    @book_4 = @library_1.books.create!(title: "Book Four", genre: "Extension")
+    @book_5 = @library_1.books.create!(title: "Book Five", genre: "Extension")
+    @book_6 = @library_1.books.create!(title: "Book Six", genre: "Extension")
+    @book_7 = @library_1.books.create!(title: "Book Seven", genre: "Extension")
+
     @author_1 = Author.create!(name: "Amanda Hugankiss", birth_year: DateTime.new(1984))
     @author_2 = Author.create!(name: "Fanny McPatdown", birth_year: DateTime.new(1950))
     @author_3 = Author.create!(name: "Mr. Evil", birth_year: DateTime.new(1969))
+    ###Extension Authors
+    @author_4 = Author.create!(name: "Sid Vicious", birth_year: DateTime.new(1984))
+    @author_5 = Author.create!(name: "Sally Buttercup", birth_year: DateTime.new(1991))
+
     @author_book_1 = AuthorBook.create!(author_id: @author_1.id, book_id: @book_1.id)
     @author_book_2 = AuthorBook.create!(author_id: @author_2.id, book_id: @book_1.id)
     @author_book_3 = AuthorBook.create!(author_id: @author_2.id, book_id: @book_2.id)
     @author_book_4 = AuthorBook.create!(author_id: @author_3.id, book_id: @book_3.id)
+    ###Extension AuthorBooks
+    @author_book5 = AuthorBook.create!(author_id: @author_4.id, book_id: @book_4.id)
+    @author_book6 = AuthorBook.create!(author_id: @author_4.id, book_id: @book_5.id)
+    @author_book7 = AuthorBook.create!(author_id: @author_5.id, book_id: @book_6.id)
+    @author_book8 = AuthorBook.create!(author_id: @author_5.id, book_id: @book_7.id)
     visit "/libraries/#{@library_1.id}"
   end
 
@@ -27,6 +43,16 @@ RSpec.describe 'the library show page' do
       expect(page).to have_link("Authors available at Richmond Public Library")
       click_link("Authors available at Richmond Public Library")
       expect(current_path).to eq("/libraries/#{@library_1.id}/authors")
+    end
+  end
+
+  it 'displays the three most popular authors available at that library' do
+    within('#three_most_popular_authors') do
+      expect(page).to have_content("Sid Vicious")
+      expect(page).to have_content("Sally Buttercup")
+      expect(page).to have_content("Fanny McPatdown")
+      expect(page).to_not have_content("Amanda Hugankiss")
+      expect(page).to_not have_content("Mr. Evil")
     end
   end
 end
