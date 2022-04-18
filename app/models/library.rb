e2c6b4c4f0a7f34789cd72d4ac5a150c810ec555
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-04-18 12:14:03 -0700

    Add: Library show page displays top 3 most poular authors

--- app/models/library.rb
+++ app/models/library.rb
@@ -7,4 +7,25 @@ class Library < ApplicationRecord
       book.authors
     end.flatten.uniq
   end
+
+  def popular_authors
+    author_names = books.map do |book|
+      book.authors.map do |author|
+        author.name
+      end
+    end.flatten
+    h = Hash.new(0)
+    author_names.each do |name|
+      if h[name] == 0
+        h[name] = 1
+      else
+        h[name] += 1
+      end
+    end
+    order = h.sort_by {|k,v| -v}
+    top_3 = order.first(3)
+    top_3.map do |pop|
+      pop[0]
+    end
+  end
 end
--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'library show page' do
     author_book_6 = AuthorsBook.create!(book_id: book_4.id, author_id: author_4.id)
 
     visit "/libraries/#{library.id}"
-    click_link "View Authors"
+    click_link "View All Authors"
     expect(current_path).to eq("/libraries/#{library.id}/authors")
     expect(page).to have_content("Archie Long Fur")
     expect(page).to have_content("Luna Twoolie")
@@ -44,4 +44,38 @@ RSpec.describe 'library show page' do
     expect(page).to_not have_content("Orange Eater")
 
   end
+
+  it 'displays three most popular authors by book number' do
+    library = Library.create!(name: 'Moscow Public Library')
+    author = Author.create!(name: 'Archie Long Fur', birth_year: 1990)
+    author_2 = Author.create!(name: 'Luna Twoolie', birth_year: 1992)
+    author_3 = Author.create!(name: 'Purple Eater', birth_year: 1990)
+    author_4 = Author.create!(name: 'Orange Eater', birth_year: 1992)
+    author_5 = Author.create!(name: 'Blue Eater', birth_year: 1992)
+    book = library.books.create!(title: "A Dogs World", genre: "Kids")
+    book_2 = library.books.create!(title: "A Cats World", genre: "Kids")
+    book_3 = library.books.create!(title: "Catepillar", genre: "Kids")
+    book_4 = library.books.create!(title: "Moth", genre: "Kids")
+    author_book_1 = AuthorsBook.create!(book_id: book.id, author_id: author.id)
+    author_book_1_2 = AuthorsBook.create!(book_id: book.id, author_id: author_2.id)
+    author_book_1_3 = AuthorsBook.create!(book_id: book.id, author_id: author_3.id)
+    author_book_1_4 = AuthorsBook.create!(book_id: book.id, author_id: author_4.id)
+    author_book_1_5 = AuthorsBook.create!(book_id: book.id, author_id: author_5.id)
+    author_book_2_1 = AuthorsBook.create!(book_id: book_2.id, author_id: author.id)
+    author_book_2_4 = AuthorsBook.create!(book_id: book_2.id, author_id: author_4.id)
+    author_book_2_5 = AuthorsBook.create!(book_id: book_2.id, author_id: author_5.id)
+    author_book_3_1 = AuthorsBook.create!(book_id: book_3.id, author_id: author.id)
+    author_book_5_4 = AuthorsBook.create!(book_id: book_3.id, author_id: author_4.id)
+
+    visit "/libraries/#{library.id}"
+
+    within "#popular-#{library.id}" do
+      expect(page).to have_content("Archie Long Fur")
+      expect(page).to have_content("Orange Eater")
+      expect(page).to have_content("Blue Eater")
+      expect(page).to_not have_content("Purple Eater")
+      expect(page).to_not have_content("Luna Twoolie")
+    end
+  end
+
 end
