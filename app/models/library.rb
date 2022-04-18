912dde85768560b15d596f14f9a4f8636c5427b3
Kim G <xiaole.guo@yahoo.com>
2022-04-18 12:31:59 -0600

    add method top_3_authors in library.rb

--- app/models/library.rb
+++ app/models/library.rb
@@ -2,4 +2,12 @@ class Library < ApplicationRecord
   validates_presence_of :name
   has_many :books
   has_many :authors, through: :books
+
+  def top_3_authors
+    authors.joins(:books)
+           .select("authors.*, count(books.*) as book_count")
+           .group('authors.id')
+           .order(book_count: :desc)
+           .limit(3)
+  end
 end
