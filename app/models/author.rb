59d464440c3a8068110e060402d5ee70cac55bc8
Sherman Au <shermau13@gmail.com>
2022-04-19 16:59:51 -0600

    feat: #age for Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,13 @@
 class Author < ApplicationRecord
   validates :name, presence: true
   validates :dob, presence: true
+  validates :deathyear, presence: true
 
 
   has_many :book_authors
   has_many :books, through: :book_authors
+
+  def age
+    deathyear.year - dob.year
+  end
 end
