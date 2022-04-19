09e27f1a3a37a3d8ae6dde34de0a9d38d761df88
Susan Boyd <sueboyd922@gmail.com>
2022-04-19 08:47:24 -0400

    Write test for average author age

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -1,31 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'books show page', type: :feature do
+  before (:each) do
+    @library = Library.create!(name: "Milton Public Library")
+
+    @book1 = @library.books.create!(title: "Eat Good Food", genre: "Travel")
+    @book2 = @library.books.create!(title: "A History of Africa", genre: "history")
+
+    @author1 = Author.create!(name: "James McFace", birth_year: "1971")
+    @author2 = Author.create!(name: "Angela Noseberry", birth_year: "1980")
+    @author3 = Author.create!(name: "Shelly Farminghoe", birth_year: "1994")
+    @author4 = Author.create!(name: "Percy Norris Cheesecake", birth_year: "1973")
+
+    BookAuthor.create!(book: @book1, author: @author1)
+    BookAuthor.create!(book: @book1, author: @author4)
+    BookAuthor.create!(book: @book2, author: @author2)
+    BookAuthor.create!(book: @book2, author: @author3)
+    BookAuthor.create!(book: @book2, author: @author4)
+  end
+
   it 'has the books name, genre and authors'  do
-    library = Library.create!(name: "Milton Public Library")
 
-    book1 = library.books.create!(title: "Eat Good Food", genre: "Travel")
-    book2 = library.books.create!(title: "A History of Africa", genre: "history")
 
-    author1 = Author.create!(name: "James McFace", birth_year: "1971")
-    author2 = Author.create!(name: "Angela Noseberry", birth_year: "1980")
-    author3 = Author.create!(name: "Shelly Farminghoe", birth_year: "1954")
-    author4 = Author.create!(name: "Percy Norris Cheesecake", birth_year: "1973")
+    visit "/books/#{@book2.id}"
+    expect(page).to have_content(@book2.title)
+    expect(page).to have_content(@book2.genre)
+    expect(page).to have_content(@author2.name)
+    expect(page).to have_content(@author3.name)
+    expect(page).to have_content(@author4.name)
+    expect(page).to have_content(@library.name)
+    expect(page).not_to have_content(@book1.title)
+    expect(page).not_to have_content(@author1.name)
+  end
 
-    BookAuthor.create!(book: book1, author: author1)
-    BookAuthor.create!(book: book1, author: author4)
-    BookAuthor.create!(book: book2, author: author2)
-    BookAuthor.create!(book: book2, author: author3)
-    BookAuthor.create!(book: book2, author: author4)
+  it 'shows the average age of the authors for the book' do
+    visit "/books/#{@book1.id}"
+    expect(page).to have_content("Authors Average Age: 50")
 
-    visit "/books/#{book2.id}"
-    expect(page).to have_content(book2.title)
-    expect(page).to have_content(book2.genre)
-    expect(page).to have_content(author2.name)
-    expect(page).to have_content(author3.name)
-    expect(page).to have_content(author4.name)
-    expect(page).to have_content(library.name)
-    expect(page).not_to have_content(book1.title)
-    expect(page).not_to have_content(author1.name)
+    visit "/books/#{@book2.id}"
+    expect(page).to have_content("Authors Average Age: 40.7")
+    expect(page).not_to have_content("Authors Average Age: 40.6667")
   end
 end
