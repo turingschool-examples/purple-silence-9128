5ffc75d46d6a210fe60cd2e1a670e79940090dc1
reuben <creubend@gmail.com>
2022-04-19 16:46:54 -0600

    feat: create library comtroller and route for show

---
+++ app/controllers/libraries_controller.rb
@@ -0,0 +1,5 @@
+class LibrariesController < ApplicationController
+    def show 
+    end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/books/:id', to: 'books#show'
+  get '/libraries/:id', to:'libraries#show'
 end
