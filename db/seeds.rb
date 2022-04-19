5a7c08de675092569ebb5e199dbd11a69cbdb415
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-19 08:00:06 -0600

    feat: book show page has name,genre, author, and library

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+library = Library.create!(name: "Denver Public Library")
+
+autho_1 = Author.create!(name: "Jon Krakauer", birth_year: 1975)
+author_2 = Author.create!(name: "Barbara Oakley, Ph.D.", birth_year: 1982)
+
+book_1 = library.books.create!(title: "Into the Wild", genre: "Non-Fiction")
+book_2 = library.books.create!(title: "a Mind for Numbers", genre: "Non-Fiction")
+
+ab_1 = AuthorBook.create!(author_id: @author_1.id, book_id: @book_1.id)
+ab_2 = AuthorBook.create!(author_id: @author_2.id, book_id: @book_2.id)
--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -3,26 +3,26 @@ require "rails_helper"
 RSpec.describe "books show page" do
   before :each do
     @library = Library.create!(name: "Denver Public Library")
-    @author = Author.create!(name: "Jon Krakauer", birth_year: 1975)
-    @author_1 = Author.create!(name: "Barbara Oakley, Ph.D.", birth_year: 1982)
-    @book = Book.create!(title: "Into the Wild", genre: "Non-Fiction")
-    @book_1 = Book.create!(title: "a Mind for Numbers", genre: "Non-Fiction")
-    @ab = AuthorBook.create!(author_id: @author.id, book_id: @book.id)
+    @author_1 = Author.create!(name: "Jon Krakauer", birth_year: 1975)
+    @author_2 = Author.create!(name: "Barbara Oakley, Ph.D.", birth_year: 1982)
+    @book_1 = @library.books.create!(title: "Into the Wild", genre: "Non-Fiction")
+    @book_2 = @library.books.create!(title: "a Mind for Numbers", genre: "Non-Fiction")
     @ab_1 = AuthorBook.create!(author_id: @author_1.id, book_id: @book_1.id)
+    @ab_2 = AuthorBook.create!(author_id: @author_2.id, book_id: @book_2.id)
 
-    visit "/books/#{@book.id}"
+    visit "/books/#{@book_1.id}"
   end
 
   it "lists the book with name and genre" do
     expect(page).to have_content("Title: Into the Wild")
-    expect(page).to have_content("Genre: Nonfiction")
+    expect(page).to have_content("Genre: Non-Fiction")
 
-    within("#book_authors") do
+    within(".book_authors") do
       expect(page).to have_content("Jon Krakauer")
-      expect(page).to have_content("Barbara Oakley, Ph.D.")
+      expect(page).to_not have_content("Barbara Oakley, Ph.D.")
     end
 
-    within("#library") do
+    within(".library") do
       expect(page).to have_content("Denver Public Library")
     end
   end
