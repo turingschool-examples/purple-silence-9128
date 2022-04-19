fc94497e7f186b021e820570fac15aa9c96cac34
Tyler Ross <tross020890@gmail.com>
2022-04-19 18:10:04 -0500

    add book model tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
 get '/books/:id', to: 'books#show'
 
+get '/libraries/:id', to: 'library#show'
+
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -11,4 +11,31 @@ RSpec.describe Book, type: :model do
     it { should have_many(:author_books) }
     it { should have_many(:authors).through(:author_books) }
   end
+
+  describe 'methods' do
+    before(:each) do
+      @library1 = Library.create!(name: "The Library")
+      @book1 = Book.create!(title: "Good Book", genre: "Mystery", library: @library1)
+      @author1 = Author.create!(name: "Good Author", age: 41)
+      @author3 = Author.create!(name: "Best Author", age: 51)
+      @author_book1 = AuthorBook.create!(author: @author1, book: @book1)
+      @author_book3 = AuthorBook.create!(author: @author3, book: @book1)
+
+      @library2 = Library.create!(name: "Cursed Library")
+      @book2 = Book.create!(title: "Bad Book", genre: "Horror", library: @library1)
+      @author2 = Author.create!(name: "Bad Author", age: 31)
+      @author_book2 = AuthorBook.create!(author: @author2, book: @book2)
+    end
+
+    it 'displays book authors' do
+
+    expect(@book1.all_authors[0].name).to eq(@author1.name)
+    expect(@book1.all_authors[1].name).to eq(@author3.name)
+    end
+    it 'displays book authors average age' do
+
+    expect(@book1.average_author_age).to eq(46)
+
+    end
+  end
 end
