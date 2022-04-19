2ba56e39e4a963f271baa78a786fb3fc63799eb2
Susan Boyd <sueboyd922@gmail.com>
2022-04-19 10:10:38 -0400

    Write most_popular_authors method for library model

--- app/models/library.rb
+++ app/models/library.rb
@@ -2,4 +2,12 @@ class Library < ApplicationRecord
   validates_presence_of :name
   has_many :books
   has_many :authors, through: :books
+  has_many :book_authors, through: :books
+
+  def most_popular_authors
+    wip = authors.joins(:books).select("authors.*, count(books) as count").group("authors.id").order(count: :desc).limit(3)
+    # require "pry"; binding.pry
+
+  end
+
 end
