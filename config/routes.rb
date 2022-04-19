d837f2a811d7028f616de6fc2f9287a8f0054db4
Susan Boyd <sueboyd922@gmail.com>
2022-04-19 09:28:11 -0400

    Add route to Library's Authors, and AuthorsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:show]
-  resources :libraries, only: [:show]
+  resources :libraries, only: [:show] do
+    resources :authors, only: [:index]
+  end
+
 end
