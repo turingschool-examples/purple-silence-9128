8ebee020b0fd51128ff5b645b6ad3e93463325a3
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-19 17:04:01 -0600

    Test: User Story 1, Book Show Page with attributes, authors, and library - complete

--- spec/features/book/show_spec.rb
+++ spec/features/book/show_spec.rb
@@ -2,7 +2,27 @@ require "rails_helper"
 
 RSpec.describe "Book Show Page", type: :feature do
   it "shows the book's attributes" do
-    book1 = Book.create!(title: "Ruby for Dummy's", genre: "Self-Help")
-    author1 = book1.author.create!(name: "Mike Dao", birth_year: 1985)
+    library = Library.create!(name: "Lakewood Public Library")
+
+    book1 = library.books.create!(title: "Ruby for Dummy's", genre: "Self-Help")
+
+    author1 = Author.create!(name: "Mike Dao", birth_year: 1985)
+    author2 = Author.create!(name: "Dani Coleman", birth_year: 1995)
+
+    book_author1 = BookAuthor.create!(book_id: book1.id, author_id: author1.id)
+    book_author2 = BookAuthor.create!(book_id: book1.id, author_id: author2.id)
+
+    visit book_path(book1[:id])
+
+    within "#book" do
+      expect(page).to have_content(book1.title)
+      expect(page).to have_content(book1.genre)
+
+      book1.authors.each do |author|
+        expect(page).to have_content(author.name)
+      end
+
+      expect(page).to have_content(book1.library.name)
+    end
   end
 end
