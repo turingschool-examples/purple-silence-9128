4dd0c19c2b6741855b557dba874ba4f01ed0bbb4
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-19 17:37:56 -0400

    Tests: Create test for existence of average author age on book show page.

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe 'Book Show Page', type: :feature do
 
   before :each do
     @library_1 = Library.create!(name: "Wake Forest Public Library")
-    @book_1 = Book.create!(title: "Eragon", genre: "Fantasy", library_id: @library_1.id)
-    @author_1 = Author.create!(name: "Christopher Paolini", birth_year: 1983)
+    @book_1 = Book.create!(title: "Dragons of Autumn Twilight", genre: "Fantasy", library_id: @library_1.id)
+    @author_1 = Author.create!(name: "Margaret Weis", birth_year: 1948)
+    @author_2 = Author.create!(name: "Tracy Hickman", birth_year: 1955)
     @author_book_1 = AuthorsBook.create!(author_id: @author_1.id, book_id: @book_1.id)
+    @author_book_2 = AuthorsBook.create!(author_id: @author_2.id, book_id: @book_1.id)
   end
 
   it 'Shows an individual book and all related attributes' do
@@ -16,7 +18,16 @@ RSpec.describe 'Book Show Page', type: :feature do
       expect(page).to have_content("Title: #{@book_1.title}")
       expect(page).to have_content("Genre: #{@book_1.genre}")
       expect(page).to have_content("Library: #{@library_1.name}")
-      expect(page).to have_content("#{@author_1.name}")
+      expect(page).to have_content(@author_1.name)
+      expect(page).to have_content(@author_2.name)
+    end
+  end
+
+  it 'Displays average age of all authors who worked on a book' do 
+    visit book_path(@book_1.id)
+
+    within("#book-#{@book_1.id}") do
+      expect(page).to have_content("Average age: 70")
     end
   end
 end
