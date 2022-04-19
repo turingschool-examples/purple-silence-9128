3aab7ef485d9e960a7767af336f0ebe9ade1c684
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-19 18:01:35 -0400

    Feature: Library show page with link to library authors view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: :show
+  
+  resources :libraries, only: :show
+  get '/libraries/:id/authors', to: 'libraries#authors'    
 end
