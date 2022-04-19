2e07c058361ed2d34b28b835882916f2e9715c3b
Sherman Au <shermau13@gmail.com>
2022-04-19 15:32:26 -0600

    test: book_authors relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_161250) do
+ActiveRecord::Schema.define(version: 2022_04_19_211421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "dob"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,5 +46,7 @@ ActiveRecord::Schema.define(version: 2022_04_11_161250) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
   add_foreign_key "books", "libraries"
 end
