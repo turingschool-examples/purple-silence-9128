b1d60fab003c337c81768c16490a4ce832ca07fa
Chris Romero <chrisleeromero1@gmail.com>
2022-04-19 15:25:24 -0600

    test: add test for user story 1, book show page displays book details

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -8,7 +8,14 @@ RSpec.describe 'book show page' do
   # And I see a list of authors for that book.
   # And I see the name of the Library it belongs to
   it 'displays the book details' do
-    
+    library = Library.create!(name: 'Denver Public Library')
+    book = library.books.create!(title: 'Foundation', genre: 'Sci-fi')
+    author = book.authors.create!(name: 'Isaac Asimov', birth_year: 1920)
+    visit "/books/#{book.id}"
+    expect(page).to have_content('Foundation')
+    expect(page).to have_content('Sci-fi')
+    expect(page).to have_content('Isaac Asimov')
+    expect(page).to have_content('Denver Public Library')
   end
 
 end
