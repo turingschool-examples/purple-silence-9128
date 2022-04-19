26259a53b8bcf5532b75b8fe4a83ea3dcfc442ce
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-19 08:17:52 -0600

    test: average author age on book show

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -13,12 +13,28 @@ RSpec.describe "Books Show" do
     author_book_3 = AuthorBook.create(author_id: author_3.id, book_id: book_2.id)
 
     visit "/books/#{book_1.id}"
-
-    expect(page).to have_content("Author One")
-    expect(page).to have_content("Author Two")
-    expect(page).to have_content("Genre1")
-    expect(page).to have_content("Library One")
+    within "#authors" do
+      expect(page).to have_content("Author One")
+      expect(page).to have_content("Author Two")
+      expect(page).to have_content("Genre1")
+      expect(page).to have_content("Library One")
+    end
   end
 
-  
+  it "shows the average of the authors" do
+    author_1 = Author.create(name: "Author One", birthyear: 1942)
+    author_2 = Author.create(name: "Author Two", birthyear: 1962)
+    author_3 = Author.create(name: "Author Three", birthyear: 1982)
+    library_1 = Library.create(name: "Library One")
+    book_1 = Book.create(title: "Book One", genre: "Genre1", library_id: library_1.id)
+    book_2 = Book.create(title: "Book Two", genre: "Genre2", library_id: library_1.id)
+    author_book_1 = AuthorBook.create(author_id: author_1.id, book_id: book_1.id)
+    author_book_2 = AuthorBook.create(author_id: author_2.id, book_id: book_1.id)
+    author_book_3 = AuthorBook.create(author_id: author_3.id, book_id: book_2.id)
+    visit "/books/#{book_1.id}"
+
+    within "#averageage" do
+        expect(page).to have_content(70)
+    end
+  end
 end
