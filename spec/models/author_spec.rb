3bda52cf2c29a476a5187798bb12b2778f046f6c
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-19 07:01:05 -0600

    test: book_spec association tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
 
   describe 'relationships' do
-    it { should have_many(:books).through(:author_books) }
     it { should have_many(:author_books)}
+    it { should have_many(:books).through(:author_books) }
   end
 end
