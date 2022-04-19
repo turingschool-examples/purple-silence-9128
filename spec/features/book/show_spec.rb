fd3e4a1b2c6b80271d9399e40c665b20eb0f0516
reuben <creubend@gmail.com>
2022-04-19 16:26:39 -0600

    test: Testing for average age displaying on show page

--- spec/features/book/show_spec.rb
+++ spec/features/book/show_spec.rb
@@ -43,9 +43,18 @@ RSpec.describe "Book Show page" do
             expect(page).to have_content("Rick")
             expect(page).to have_content("Tom")
         end
-        save_and_open_page
         expect(page).to have_content("Library: DCL")
- 
     end
 
+    it 'displays the average age of the authors'do 
+        lib = Library.create!(name: "DCL")
+        @book1 = lib.books.create!(title: "Where Red Fern", genre: 'fiction')
+        @author1 = Author.create!(name: "Rick", birth_year: 1990)
+        @author2 = Author.create!(name: "Tom", birth_year: 1992)
+        BookAuthor.create(book_id: @book1.id, author_id: @author1.id)
+        BookAuthor.create(book_id: @book1.id, author_id: @author2.id)
+
+        visit "/books/#{@book1.id}"
+        expect(page).to have_content("Average age: 30")
+    end 
 end
