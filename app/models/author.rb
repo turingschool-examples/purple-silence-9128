b88939aa5e3b3df7589fef538feb56c5f07908ab
Luke Pascale <lpluke5@gmail.com>
2022-04-19 14:24:17 -0500

    Story_1 final commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
   has_many :author_books
   has_many :books, through: :author_books
+
+  def average_age
+    
+  end
 end
--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -5,19 +5,27 @@ RSpec.describe 'the Book show page' do
     @library = Library.create!(name: "DPLibrary")
     @book = @library.books.create!(title: "There and Back Again", genre: 'Fantasy')
     @author = @book.authors.create!(name: "JRR Tolkein", birth_year: 1892)
+    @author = @book.authors.create!(name: "Brando Sando", birth_year: 1967)
     @book.author_books.create!(book_id: @book.id, author_id: @author.id)
   end
 
   it 'displays the books name and genre' do
     visit "/libraries/#{@library.id}/books/#{@book.id}"
-    save_and_open_page
 
     expect(page).to have_content(@book.title)
     expect(page).to have_content(@book.genre)
-    expect(page).to have_content(@library.name)
   end
 
-  it 'displays a list of authors for that book'
+  it 'displays a list of authors for that book' do
+    visit "/libraries/#{@library.id}/books/#{@book.id}"
 
-  it 'displays the library the book belongs to'
+    expect(page).to have_content("JRR Tolkein")
+    expect(page).to have_content("Brando Sando")
+  end
+
+  it 'displays the library the book belongs to' do
+    visit "/libraries/#{@library.id}/books/#{@book.id}"
+
+    expect(page).to have_content(@library.name)
+  end
 end
