b7941f536ada040fb5ebd78bc692d08bbbf0c5f4
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-19 07:03:46 -0600

    feature: model tests all passing

--- spec/models/author_books_spec.rb
+++ spec/models/author_books_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe AuthorBook, type: :model do
 
   describe 'relationships' do
-    it { should belong_to(:books)}
-    it { should belong_to(:authors)}
+    it { should belong_to(:book)}
+    it { should belong_to(:author)}
   end
 end
