caa1374eca7be5fe85e953a9468cb078cfe5f48a
Sherman Au <shermau13@gmail.com>
2022-04-19 15:31:03 -0600

    test: author validations and relationships

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :dob }
+  end
+
+  describe 'relationships' do
+    it { should have_many :book_authors }
+    it { should have_many :books }
+  end
+end
