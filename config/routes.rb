5d46535b69407f507e39f2296f4f594805ae5506
Joseph Downs <jos.w.downs@gmail.com>
2022-04-19 15:36:20 -0600

    Round 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/books/:id", to: "books#show"
+
+  get "/libraries/:id", to: "libraries#show"
+
+  get "/libraries/:id/authors", to: "library_authors#index"
 end
