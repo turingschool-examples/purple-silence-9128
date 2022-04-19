697a4ff3b7cf1cd82a74a6a4bc70ae571290dd62
Sherman Au <shermau13@gmail.com>
2022-04-19 16:21:05 -0600

    test: shows the name of the library the book belongs too

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -88,7 +88,7 @@ describe 'book show page' do
       end
     end
 
-    xit 'shows the name of the library the book belongs too' do
+    it 'shows the name of the library the book belongs too' do
       within "#library" do
         expect(page).to have_content("Avaliable at: Greenie")
 
