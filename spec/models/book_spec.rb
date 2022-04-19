b13397ca09b2fa2313674dc6d616f9aeb538d17d
Tyler Ross <tross020890@gmail.com>
2022-04-19 16:34:51 -0500

    Feat: setup and tested relationships

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe Book, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :genre }
   end
-  
+
   describe 'relationships' do
-    it { should belong_to :library }
-  end 
+    it { should belong_to(:library) }
+    it { should have_many(:author_books) }
+    it { should have_many(:authors).through(:author_books) }
+  end
 end
