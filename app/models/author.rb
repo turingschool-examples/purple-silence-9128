dbd8fa1b8efc2c3ab54b87983acc625e0a0994bb
Joseph Senecal <jasenecal2@gmail.com>
2022-04-18 20:55:21 -0600

    Add Author model relationships and validations

---
+++ app/models/author.rb
@@ -0,0 +1,7 @@
+class Author <ApplicationRecord
+  has_many: :book_authors
+  has_many: :books, through: :book_authors
+  has_many: :libraries, through: :books
+
+  validates_presence_of: :name, :age
+end
